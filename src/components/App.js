4a0a752da0f648d44a0750db0b8a719ef1d9a804
IvyW2 <ivywambugha2@gmail.com>
2023-01-27 02:28:19 +0300

    build 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-// import logo from './logo.svg';
-// import { Container } from 'react-bootstrap'
 import {Routes, Route}  from 'react-router-dom';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from './Landing';
 import NavbarCustom from './Navbar';
-// import ProjectItem from '../ProjectItem';
+
 import Profile from './profile';
 import ProjectItem from './ProjectItem';
 
